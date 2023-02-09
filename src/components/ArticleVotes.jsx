3d0fc902e176d870b4b856c925a137b8770125bf
Lee Hamilton <lphamilton87@gmailcom>
2023-02-09 08:55:11 +0000

    task 7 complete for review - update votes functionality

---
+++ src/components/ArticleVotes.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { patchArticleVotesById } from "../utils/api";
+
+const Votes = ({ article_id, votes }) => {
+  console.log(votes);
+  const [votesChange, setVotesChangeBy] = useState(0);
+
+  const increaseVotes = () => {
+    setVotesChangeBy((currChange) => currChange + 1);
+    patchArticleVotesById(article_id, 1);
+  };
+
+  const dereaseVotes = () => {
+    setVotesChangeBy((currChange) => currChange - 1);
+    patchArticleVotesById(article_id, -1);
+  };
+
+  return (
+    <>
+      <h6>{votes + votesChange} Votes</h6>
+      <button disabled={votesChange === -1} onClick={() => dereaseVotes()}>
+        -1
+      </button>
+      <button disabled={votesChange === +1} onClick={() => increaseVotes()}>
+        +1
+      </button>
+    </>
+  );
+};
+
+export default Votes;
